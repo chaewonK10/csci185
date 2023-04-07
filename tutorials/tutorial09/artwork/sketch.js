ccabeb8c178defb48a177b8e54916d52494c6462
chaewonK10 <chaewonkim1015@gmail.com>
2023-04-07 10:46:36 -0400

    tutorial09

---
+++ tutorials/tutorial09/artwork/sketch.js
@@ -0,0 +1,86 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+const colors = ["#08415c", "#e1f0c4", "#6bab90", "#55917f", "#5e4c5a"];
+
+function randomFloat(min, max) { 
+	// min and max included 
+	return Math.random() * (max - min) + min;
+}
+
+function randomInt(min, max) { 
+	// min and max included 
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    frameRate(3);
+    
+    
+
+    
+
+   
+
+    
+}
+
+
+
+
+
+// // anything that you want to animate goes in this function:  
+function draw() {
+    clear();
+    let i=0;
+    while (i < 6) {
+    
+
+    // draw a random circle:
+    let x = randomInt(0, canvasWidth);
+    let y = randomInt(0, canvasHeight);
+    let size = randomFloat(25, 60);
+
+    if (i % 3 === 0) {
+    fill("skyblue");
+    circle(x, y, size);
+    }
+
+    if (i % 3 === 1) {
+    fill("yellow");
+    square(x, y, size);
+    }
+
+    if (i % 3 === 2) {
+    fill("orange");
+    triangle(
+            x, y - size / 2, 
+            x - size / 2.25, y + size / 4.3,
+            x + size / 2.25, y + size / 4.3
+        );
+    }
+        
+    ++i
+    }
+
+}
+
+
+ 
+// // 1. draw a random square:
+// square(x, y, size);
+
+
+
+// // 2. draw a random triangle:
+// triangle(
+//     x, y - size / 2, 
+//     x - size / 2.25, y + size / 4.3,
+//     x + size / 2.25, y + size / 4.3
+// );
+
+// // 3. draw a random line:
+// let coefX = randomFloat(-3, 3);
+// let coefY = randomFloat(-3, 3);
+// line(x, y, x + size * coefX, y + size * coefY);
+
